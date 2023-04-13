294c395b764fc8e37bdb29da5e8956a51daf2ca9
ayuaprliia <ayuagungaprilia@gmail.com>
2023-04-13 22:51:59 +0800

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
+        //membuat tampilan awal
         public static void main(String[] args) {
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("|=================WELCOME!==================|");
                 System.out.println("|               FOOD RUSH                   |");
                 System.out.println("|------YOUR NO. 1 FOOD DELIVERY SERVICE-----|");
                 System.out.println("|===========================================|");
-                System.out.println("Press any key to continue...");
-                scanner.nextLine();
+                System.out.println("Press any key to continue..."); //untuk lanjut ke menu berikutnya
+                scanner.nextLine(); //membaca input user
                 System.out.println("Please Wait...");
+
         }
 }
