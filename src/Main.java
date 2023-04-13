767208935ac8eb5b4ae557cedc5c81f0a9c850d3
ayuaprliia <ayuagungaprilia@gmail.com>
2023-04-13 21:14:05 +0800

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,13 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
-                Scanner input = new Scanner(System.in);
-                int pilihan = 0;
-
-                while (pilihan != 4) {
-                        System.out.println("=================WELCOME!==================");
-                        System.out.println("                FOOD RUSH                 ");
-                        System.out.println("------YOUR NO. 1 FOOD DELIVERY SERVICE-----");
-                        System.out.println("===========================================");
-                        System.out.println("|1. Tambah data");
-                        System.out.println("|2. Hapus data");
-                        System.out.println("|3. Lihat data");
-                        System.out.println("4. Keluar");
-                        System.out.print("Pilihan Anda: ");
-                        pilihan = input.nextInt();
-
-                        switch (pilihan) {
-                                case 1:
-                                        System.out.println("Anda memilih tambah data.");
-                                        break;
-                                case 2:
-                                        System.out.println("Anda memilih hapus data.");
-                                        break;
-                                case 3:
-                                        System.out.println("Anda memilih lihat data.");
-                                        break;
-                                case 4:
-                                        System.out.println("Terima kasih.");
-                                        break;
-                                default:
-                                        System.out.println("Pilihan tidak valid.");
-                                        break;
-                        }
-
-                        System.out.println();
-                }
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("|=================WELCOME!==================|");
+                System.out.println("|               FOOD RUSH                   |");
+                System.out.println("|------YOUR NO. 1 FOOD DELIVERY SERVICE-----|");
+                System.out.println("|===========================================|");
+                System.out.println("Press any key to continue...");
+                scanner.nextLine();
+                System.out.println("Please Wait...");
         }
 }
